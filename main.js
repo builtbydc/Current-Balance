9e5a9e0aed3e455d54fc88e60de65a3d007dc5a9
builtbydc <dcarl026@fiu.edu>
2024-02-15 18:42:01 -0700

    massive overhaul

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let MONEY_SECTION_ID = "money";
-let CREDIT_CARDS_SECTION_ID = "credit-cards";
+// UTILITY FUNCTIONS
+////////////////////
 
 function 
 forceNumber(val) 
@@ -14,11 +14,8 @@ function
 numberToUSD(val) 
 {
     let cents = Math.round(100*val) + ""; 
-    if(cents.length === 1) {
-        cents = "00" + cents;
-    } else if(cents.length === 2) {
-        cents = "0" + cents;
-    }
+    cents = "0".repeat(Math.max(0, 3 - cents.length)) + cents;
+
     // cents is now number of cents as string
     let usd = "$" + cents.substring(0, cents.length - 2) + "." +
               cents.substring(cents.length - 2);
@@ -29,275 +26,237 @@ numberToUSD(val)
 function
 USDToNumber(val) 
 {
-    // always will be a number
-    return val.substring(1) - 0;
+    return forceNumber(val.substring(1));
+}
+
+// CACHE MANIPULATION
+/////////////////////
+
+function
+saveCache() 
+{
+    let cacheData = [];
+    cacheData.push(cache.length);
+    for(let map of cache) {
+        cacheData.push(map.size);
+    }
+
+    for(let map of cache) {
+        for(let key of map.keys()) {
+            cacheData.push(key, map.get(key));
+        }
+    }
+
+    localStorage.setItem(CACHE_STORAGE_ID, cacheData);
+}
+
+function
+retrieveCache() {
+    let rawCacheData = localStorage.getItem(CACHE_STORAGE_ID);
+    if(rawCacheData === null) return [];
+
+    let cacheData = rawCacheData.split(",");
+    console.log(cacheData);
+    let tempCache = [];
+
+    let numberOfSections = forceNumber(cacheData[0]);
+    let numberOfEntries = [];
+    for(let i = 1; i < 1 + numberOfSections; i++) {
+        tempCache.push(new Map());
+        numberOfEntries.push(forceNumber(cacheData[i]));
+    }
+
+    let i = 1 + numberOfSections;
+    for(let n = 0; n < numberOfEntries.length; n++) {
+        tempCache[n].set(cacheData[i++], cacheData[i++]);
+        tempCache[n].set(cacheData[i++], cacheData[i++] === "true");
+        tempCache[n].set(cacheData[i++], cacheData[i++]);
+        for(let j = 0; j < numberOfEntries[n] - 3; j++) {
+            tempCache[n].set(cacheData[i++], forceNumber(cacheData[i++]));
+        }
+    }
+
+    return tempCache;
+}
+
+// OPENING DIALOGS & ADDING TO PAGE
+///////////////////////////////////
+
+function 
+openDialog(id) 
+{
+    document.getElementById(id).showModal();
+}
+
+function
+closeDialog(id) 
+{
+    document.getElementById(id).close();
+}
+
+function
+addSection() 
+{
+    let dialog = document.getElementById("add-section-dialog");
+    let sectionMap = new Map();
+    sectionMap.set("#title", dialog.children[0].children[1].value);
+    sectionMap.set("#operation", dialog.children[1].children[1].children[0].checked);
+    sectionMap.set("#color", dialog.children[2].children[1].value);
+    console.log(dialog.children[2].children[1].value);
+
+    cache.push(sectionMap);
+    dialog.close();
+    saveCache();
+    refreshPage();
+}
+
+function
+addEntry(i, dialogID) 
+{
+    let dialog = document.getElementById(dialogID);
+    let key = dialog.children[0].children[1].value + "|" + dialog.children[1].children[1].value;
+    cache[i].set(key, 0);
+    closeDialog(dialogID);
+    saveCache();
+    refreshPage();
 }
 
 function 
-updateTotal(id) 
+updateTotal() 
 {
-    let section = document.getElementById(id);
-    let entries = section.children;
-    let indexOfTotalRow = section.children.length - 1;
+    let total = 0;
+    for(let i = 0; i < cache.length; i++) {
+        let sectionTotalSpanID = "section-" + i + "-total-span";
 
-    let valueList = [];
-    let storageID = id;
+        let sectionTotal = 0;
+        for(let key of cache[i].keys()) {
+            if(sectionKeywords.has(key)) {
+                continue;
+            }
 
-    let total = 0.0;
-    for(let i = 1; i < indexOfTotalRow; i++) {
-        // index 0 is the heading, skip it
-        //stop before the total row
+            sectionTotal += cache[i].get(key);
+        }
 
-        let val = entries[i].children[1].children[0].value;
-        // children[1] is the input field
+        document.getElementById(sectionTotalSpanID).children[1].innerText = numberToUSD(sectionTotal);
+        if(cache[i].get("#operation")) {
+            total += sectionTotal;
+        } else {
+            total -= sectionTotal;
+        }
+
+    }
+    document.getElementById("current-balance").innerText = numberToUSD(total);
+    saveCache();
+}
+
+function
+removeEntry(i, key)
+{
+    cache[i].delete(key);
+    saveCache();
+    refreshPage();
+}
+
+function
+refreshPage()
+{
+    console.log(cache);
+    let content = document.getElementById("content");
+    let dialogs = document.getElementById("dialogs");
+    content.innerHTML = "";
+    dialogs.innerHTML = "";
+
+    let sectionHTML = document.getElementById("section-template").innerHTML;
+    let entrySpanHTML = document.getElementById("entry-span-template").innerHTML;
+    let totalSpanHTML = document.getElementById("total-span-template").innerHTML;
+    let addEntryDialogHTML = document.getElementById("add-entry-dialog-template").innerHTML;
+
+    for(let i = 0; i < cache.length; i++) {
+        content.innerHTML += sectionHTML;
+        let section = content.children[i];
+        let sectionID = "section-" + i;
+        section.id = sectionID;
+        section.children[0].children[0].innerText = cache[i].get("#title");
+        section.style = "color:" + cache[i].get("#color") + ";";
+
+        dialogs.innerHTML += addEntryDialogHTML;
+        let dialog = dialogs.children[i];
+        let dialogID = sectionID + "-add-entry-dialog";
+        dialog.id = dialogID;
         
-        total += forceNumber(val);
 
-        valueList.push(forceNumber(val));
-    }
-
-    document.getElementById(id + "-total").innerText = numberToUSD(total);
-    updateCurrentBalance();
-
-    localStorage.setItem(storageID, valueList);
-    
-    return total;
-}
-
-function
-updateCurrentBalance() 
-{
-    let moneyTotal = USDToNumber(document.getElementById("money-total").innerText);
-    let creditCardTotal = USDToNumber(document.getElementById("credit-cards-total").innerText);
-
-    document.getElementById("current-balance").innerText = numberToUSD(moneyTotal - creditCardTotal);
-}
-
-function 
-toggleSection(id) 
-{
-    let section = document.getElementById(id);
-    let entries = section.children;
-    let indexOfTotalRow = section.children.length - 1;
-
-    let flag = entries[1].style.display !== "none";
-
-    for(let i = 1; i < indexOfTotalRow; i++) {
-        section.children[i].style.display = flag ? "none" : "inline-flex";
-    }
-
-    let heading = document.getElementById(id + "-heading");
-    let label = heading.innerText.substring(0, heading.innerText.length - 1);
-
-    heading.innerText = flag ? label + "▼" : label + "▲";
-}
-
-function drawEntryRow(name, bank, parentID) {
-    let html = "<span class='entry'>\
-                    <div>\
-                        <h3>" + name + "</h3>\
-                        <p>" + bank + "</p>\
-                    </div>\
-                    <div class='toolbox'>\
-                        <input type='number' step='0.01' onkeyup='updateTotal(\"" + parentID + "\")'>\
-                        <button onclick='removeEntry(\"" + name + bank + "\", \"" + parentID + "\")'  class='tool plus'>×</button>\
-                    </div>\
-                </span>";
-
-    document.getElementById(parentID).innerHTML += html;
-}
-
-function drawTotalRow(parentID) {
-    let html = "<span class='entry'>\
-                    <div>\
-                        <h3>Total</h3>\
-                    </div>\
-                    <p id='" + parentID + "-total'>$0.00</p>\
-                </span>";
-    
-    document.getElementById(parentID).innerHTML += html;
-}
-
-function drawMoneyRow(name, bank) {
-    drawEntryRow(name, bank, MONEY_SECTION_ID);
-}
-
-function drawCreditCardsRow(name, bank) {
-    drawEntryRow(name, bank, CREDIT_CARDS_SECTION_ID);
-}
-
-// load from storage
-
-function
-loadSectionDataFromStorage(id)
-{
-    let storageID = id;
-
-    let item = localStorage.getItem(storageID);
-    if(item === null) return;
-
-    let array = item.split(",");
-
-    let section = document.getElementById(id);
-    let entries = section.children;
-    let indexOfTotalRow = section.children.length - 1;
-
-    for(let i = 1; i < indexOfTotalRow; i++) {
-        let val = array[i-1];
-        if(forceNumber(val) !== 0) entries[i].children[1].children[0].value = val;
-    }
-}
-
-function 
-loadEntriesFromStorage(id) 
-{
-    let storageID = id + "-entries";
-
-    let item = localStorage.getItem(storageID);
-    if(item === null) return [];
-
-    let array = item.split(",");
-
-    let entries = [];
-    for(let i = 0; i < array.length; i += 2) {
-        entries.push([array[i], array[i+1]]);
-    }
-
-    return entries;
-}
-
-function 
-addEntry(name, bank, entries, id) 
-{
-    let storageID = id + "-entries";
-
-    entries.push([name, bank]);
-    localStorage.setItem(storageID, entries);
-    //must add to data
-    refresh();
-}
-
-function removeEntry(tag, id) {
-    let storageID = id + "-entries";
-    if(id === MONEY_SECTION_ID) {
-        for(let i = 0; i < moneyEntries.length; i++) {
-            if(tag === moneyEntries[i][0] + moneyEntries[i][1]) {
-                if(moneyEntries.length === 1) {
-                    moneyEntries = [];
-                    localStorage.removeItem(storageID);
-                } else {
-                    moneyEntries.splice(i, 1);
-                    localStorage.setItem(storageID, moneyEntries);
-
-                    let section = document.getElementById(MONEY_SECTION_ID);
-                    let entries = section.children;
-                    let indexOfTotalRow = entries.length - 1;
-                    for(let j = i+1; j < indexOfTotalRow - 1; j++) {
-                        entries[j].children[1].children[0].value = entries[j+1].children[1].children[0].value;
-                    }
-                }
-                break;
+        let j = 0;
+        for(let key of cache[i].keys()) {
+            if(sectionKeywords.has(key)) {
+                continue;
             }
-        }
-    } else if(id === CREDIT_CARDS_SECTION_ID) {
-        for(let i = 0; i < creditCardsEntries.length; i++) {
-            if(tag === creditCardsEntries[i][0] + creditCardsEntries[i][1]) {
-                if(creditCardsEntries.length === 1) {
-                    creditCardsEntries = [];
-                    localStorage.removeItem(storageID);
-                } else {
-                    creditCardsEntries.splice(i, 1)
-                    localStorage.setItem(storageID, creditCardsEntries);
-
-                    let section = document.getElementById(CREDIT_CARDS_SECTION_ID);
-                    let entries = section.children;
-                    let indexOfTotalRow = entries.length - 1;
-                    for(let j = i+1; j < indexOfTotalRow - 1; j++) {
-                        entries[j].children[1].children[0].value = entries[j+1].children[1].children[0].value;
-                    }
-                }
-                break;
+            console.log(cache[i].keys())
+            console.log(key);
+            let labels = key.split("|");
+            let val = cache[i].get(key);
+            section.innerHTML += entrySpanHTML;
+            let entry = section.children[1 + j];
+            entry.children[0].children[0].children[0].innerText = labels[0];
+            entry.children[0].children[1].innerText = labels[1];
+            if(val !== 0) {
+                console.log(entry.children[1].children[0]);
+                entry.children[1].children[0].value = val;
             }
+
+            j++;
         }
+
+        section.innerHTML += totalSpanHTML;
+        section.children[1+j].id = sectionID + "-total-span"
     }
 
-    updateTotal(id);
-    refresh();
-    
-}
+    for(let i = 0; i < cache.length; i++) {
+        let section = content.children[i];
+        let sectionID = "section-" + i;
+        let dialog = dialogs.children[i];
+        let dialogID = sectionID + "-add-entry-dialog"
 
-let moneyEntries;
-let creditCardsEntries;
+        dialog.children[2].children[0].addEventListener("click", function() {
+            closeDialog(dialogID);
+        });
+        dialog.children[2].children[1].addEventListener("click", function() {
+            addEntry(i, dialogID);
+        });
 
-let moneyEntryDialog = document.getElementById("add-money-entry");
-function 
-openMoneyEntryDialog() 
-{
-    moneyEntryDialog.showModal();
-}
+        section.children[0].children[1].addEventListener("click", function() {
+            openDialog(dialogID);
+        });
 
-function
-closeMoneyEntryDialog()
-{
-    moneyEntryDialog.close();
-}
+        let j = 0;
+        for(let key of cache[i].keys()) {
+            if(sectionKeywords.has(key)) {
+                continue;
+            }
+            let entry = section.children[1 + j];
 
-function 
-addMoneyEntry() 
-{
-    let name = moneyEntryDialog.children[0].children[1].value;
-    let bank = moneyEntryDialog.children[1].children[1].value;
-    moneyEntryDialog.close();
-    addEntry(name, bank, moneyEntries, MONEY_SECTION_ID);
-}
+            let val = cache[i].get(key);
+            if(val !== 0) {
+                entry.children[1].children[0].value = val;
+            }
 
-let creditCardsEntryDialog = document.getElementById("add-credit-cards-entry");
-function
-openCreditCardsEntryDialog() 
-{
-    creditCardsEntryDialog.showModal();
-}
+            entry.children[1].children[0].addEventListener("keyup", function() {
+                cache[i].set(key, forceNumber(entry.children[1].children[0].value));
+                updateTotal();
+            });
 
-function
-closeCreditCardsEntryDialog()
-{
-    creditCardsEntryDialog.close();
-}
+            entry.children[1].children[1].addEventListener("click", function() {
+                removeEntry(i, key);
+            })
 
-function
-addCreditCardsEntry() 
-{
-    let name = creditCardsEntryDialog.children[0].children[1].value;
-    let bank = creditCardsEntryDialog.children[1].children[1].value;
-    creditCardsEntryDialog.close();
-    addEntry(name, bank, creditCardsEntries, CREDIT_CARDS_SECTION_ID);
-}
+            j++;
+        }
 
-function 
-refresh() 
-{
-    document.getElementById(MONEY_SECTION_ID).innerHTML = baseMoneyHTML;
-    document.getElementById(CREDIT_CARDS_SECTION_ID).innerHTML = baseCreditCardsHTML;
-
-    moneyEntries = loadEntriesFromStorage("money");
-    creditCardsEntries = loadEntriesFromStorage("credit-cards");
-
-    for(let i = 0; i < moneyEntries.length; i++) {
-        drawMoneyRow(moneyEntries[i][0], moneyEntries[i][1]);
     }
-    drawTotalRow(MONEY_SECTION_ID);
-    for(let i = 0; i < creditCardsEntries.length; i++) {
-        drawCreditCardsRow(creditCardsEntries[i][0], creditCardsEntries[i][1]);
-    }
-    drawTotalRow(CREDIT_CARDS_SECTION_ID);
-
-    loadSectionDataFromStorage(MONEY_SECTION_ID);
-    loadSectionDataFromStorage(CREDIT_CARDS_SECTION_ID);
-    
-    updateTotal(MONEY_SECTION_ID);
-    updateTotal(CREDIT_CARDS_SECTION_ID);
 }
 
-let baseMoneyHTML = document.getElementById(MONEY_SECTION_ID).innerHTML;
-let baseCreditCardsHTML = document.getElementById(CREDIT_CARDS_SECTION_ID).innerHTML;
-refresh();
+let sectionKeywords = new Set(["#title", "#operation", "#color"]);
+
+let CACHE_STORAGE_ID = "cache-storage";
+let cache = retrieveCache();
+refreshPage();
+updateTotal();
